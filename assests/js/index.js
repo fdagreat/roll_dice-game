fc51d79918d388d7bfd5a549458413a39881e0ae
Fredy German <fredgerman30@gmail.com>
2021-05-06 21:04:58 +0300

    Added Current Round Message

--- assests/js/index.js
+++ assests/js/index.js
@@ -3,6 +3,7 @@
 let player1Score = 0;
 let player2Score = 0;
 let plater1Turn = true;
+let round = 1;
 
 //create variables to store DOM node
 
@@ -11,6 +12,7 @@ const player2Dice = document.getElementById('player2Dice');
 
 const player1Scoreboard = document.getElementById('player1Scoreboard');
 const player2Scoreboard = document.getElementById('player2Scoreboard');
+const roundMessage = document.getElementById('round');
 const message = document.getElementById('message');
 const rollBtn = document.getElementById('rollBtn');
 const resetBtn = document.getElementById('resetBtn');
@@ -24,6 +26,8 @@ function resetgame(){
     message.textContent = "Player 1 Turn"
     player1Scoreboard.textContent = 0
     player2Scoreboard.textContent = 0
+    round = 1
+    roundMessage.textContent = `Round:${round}`
     player1Dice.textContent = '-'
     player2Dice.textContent = '-'
     player1Score = 0
@@ -39,6 +43,7 @@ rollBtn.addEventListener('click', function(){
     let randNumber =2*(Math.floor(Math.random()*6)+1);
    
     if(plater1Turn){
+    
     player1Score +=randNumber;
     player1Dice.textContent = randNumber;
     player1Scoreboard.textContent = player1Score;
@@ -54,12 +59,36 @@ rollBtn.addEventListener('click', function(){
         message.textContent = "Player 1 Turn";
     }
 
+    round += 0.5;
+   
+   
+
+        if (round == 1) {
+             console.log(round)
+            roundMessage.textContent = `Round:${round}`
+        }
+           
+
+        if (round == 2) {
+            console.log(round)
+            roundMessage.textContent = `Round:${round}`
+        }
+
+
+        if (round == 3) {
+            console.log(round)
+            roundMessage.textContent = `Round:${round}`
+        }
+
+
     if(player1Score >=20){
         message.textContent = "Player 1 has won! 🥇";
+        roundMessage.textContent = ``
         toggleButtons();
               
     }else if(player2Score >=20){
         message.textContent = "Player 2 has won! 🥇";
+        roundMessage.textContent = ``
         toggleButtons();
     }
 
